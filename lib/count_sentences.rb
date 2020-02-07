2fbbc72688efcd78f717b25e8f8a140ee1adfa19
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-07 05:18:35 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true 
+    else 
+      false 
+    end 
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    else 
+      false 
+    end 
   end
 
   def exclamation?
-
+     if self.end_with?("!")
+      return true 
+    else 
+      false 
+    end 
   end
 
-  def count_sentences
-
+  def count_sentences #grep was used to include spaces in array for the sentence count. 
+    new_array = self.split(/[.?!]/).grep(/\S/).count
   end
 end
